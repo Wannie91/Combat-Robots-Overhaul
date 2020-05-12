05a3e12e3cc8d5186f7dd1374f652a7ef034ce58
Wannie <wannie.wanner@hotmail.com>
2020-05-12 23:39:31 +0200

    Add files via upload

--- data-updates.lua
+++ data-updates.lua
@@ -6,7 +6,9 @@ data.raw.capsule["destroyer-capsule"].enabled = false
 data.raw.capsule["defender-capsule"].enabled = false
 data.raw.capsule["distractor-capsule"].enabled = false
 
-
+data.raw["combat-robot"]["destroyer"].enabled = false
+data.raw["combat-robot"]["defender"].enabled = false
+data.raw["combat-robot"]["distractor"].enabled = false
 
 data.raw["technology"]["combat-robotics"].effects = { { type = "unlock-recipe", recipe = "destroyer-unit" },
 													  { type = "unlock-recipe", recipe = "defender-unit" },
